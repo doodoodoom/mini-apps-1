7285c6602fa116ebd37dfec2c4e733d4bb56e04e
doodoodoom <madridmatt17@gmail.com>
2019-01-28 17:27:17 -0800

    Initialize app.js, index.html for challenge 1

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,30 @@
+var ticTacToe = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
+var currentPlayer = 'X';
 
+var victoryMessage = 'CONGRATS ' + currentPlayer + '! YOU WON!!'
+var drawMessage = 'NO ONE WON!!';
+
+var gameOver = false;
+
+var playerTurn = (currentPlayer, ticTacToe) => {
+  
+};
+
+var checkForGameOver = () => {
+
+};
+
+var resetGame = () => {
+  currentPlayer = 'X';
+  ticTacToe = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+};
+
+var changeSquare = (currentPlayer, square) => {
+  if (currentPlayer === 'X') {
+    square = 1;
+  } else if (currentPlayer === 'O') {
+    square = -1;
+  }
+
+};
